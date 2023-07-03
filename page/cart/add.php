871a0e14c36aa3108587ef6cd3a9f48435d7f13d
Ahmad Zaidaan <zaidaan2019@gmail.com>
2023-07-03 14:16:30 +0700

    fixed quantity when adding new products

--- page/cart/add.php
+++ page/cart/add.php
@@ -10,18 +10,27 @@ if (isset($_GET['product_id']) && isset($_GET['quantity'])) {
 }
 
 $user_email = $_SESSION['email'];
-$result = get('user', 'WHERE email="'.$user_email.'"');
+$result = get('user', 'WHERE email="' . $user_email . '"');
 $data = mysqli_fetch_assoc($result);
 
 $user_id = $data['user_id'];
 
-$result = insert('cart', [
-	'user_id' => $user_id,
-	'product_id' => $product_id,
-	'quantity' => $quantity
-]);
+$get_cart = get('cart', 'WHERE user_id=' . $user_id . ' AND product_id=' . $product_id);
+if (mysqli_num_rows($get_cart) > 0) {
+	$data_cart = mysqli_fetch_assoc($get_cart);
+
+	$cart_quantity = $data_cart['quantity'];
+
+	$query = 'UPDATE cart SET quantity="' . $cart_quantity + $quantity . '" WHERE product_id=' . $product_id;
+	$result = mysqli_query($connect, $query);
+} else {
+	$result = insert('cart', [
+		'user_id' => $user_id,
+		'product_id' => $product_id,
+		'quantity' => $quantity
+	]);
+}
 
 if ($result) {
 	echo '<script>window.location.href = "index.php?page=cart_list"</script>';
 }
-?>
