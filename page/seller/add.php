a7b6fc240107b2ffb28758ddbe2ae6dddbcd5508
Ahmad Zaidaan <zaidaan2019@gmail.com>
2023-07-03 21:21:33 +0700

    added seller

---
+++ page/seller/add.php
@@ -0,0 +1,241 @@
+<?php
+check('login');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="Ansonika">
+	<title>Allaia | Bootstrap eCommerce Template - ThemeForest</title>
+
+	<!-- Favicons-->
+	<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+	<link rel="apple-touch-icon" type="image/x-icon" href="img/apple-touch-icon-57x57-precomposed.png">
+	<link rel="apple-touch-icon" type="image/x-icon" sizes="72x72" href="img/apple-touch-icon-72x72-precomposed.png">
+	<link rel="apple-touch-icon" type="image/x-icon" sizes="114x114" href="img/apple-touch-icon-114x114-precomposed.png">
+	<link rel="apple-touch-icon" type="image/x-icon" sizes="144x144" href="img/apple-touch-icon-144x144-precomposed.png">
+
+	<!-- GOOGLE WEB FONT -->
+	<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900&display=swap" rel="stylesheet">
+
+	<!-- BASE CSS -->
+	<link href="css/bootstrap.custom.min.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+
+	<!-- SPECIFIC CSS -->
+	<link href="css/cart.css" rel="stylesheet">
+
+	<!-- YOUR CUSTOM CSS -->
+	<link href="css/custom.css" rel="stylesheet">
+
+</head>
+
+<body>
+	<div id="page">
+		<main class="bg_gray">
+			<div class="container margin_30">
+				<div class="page_header">
+					<div class="breadcrumbs">
+						<ul>
+							<li><a href="#">Home</a></li>
+							<li><a href="#">Category</a></li>
+							<li>Page active</li>
+						</ul>
+					</div>
+					<h1 class="pt-3">Add Product</h1>
+				</div>
+				<form action="" method="POST" enctype="multipart/form-data">
+					<div class="container pb-5">
+						<div class="row">
+							<div class="col-3">
+								<div>
+									<img src="uploads/product/default.jpg" alt="default" width="100%">
+								</div>
+								<div class="mt-3">
+									<label class="form-label">Product Image</label>
+									<input class="form-control" type="file" name="image">
+								</div>
+							</div>
+							<div class="col-9">
+								<ul style="list-style: none;" class="pl-0">
+									<li class="mb-2">
+										<label class="form-label">Product Name</label>
+										<input type="text" name="name" class="form-control" required>
+									</li>
+									<div class="row">
+										<div class="col-4">
+											<label class="form-label">Category</label>
+											<select id="list-category" class="form-control form-select" name="category">
+												<option selected disabled hidden>-</option>
+												<?php
+												$result = get('category');
+												foreach ($result as $data) :
+													$category_id = $data['category_id'];
+													$category_name = $data['category_name'];
+												?>
+													<option value="<?= $category_id ?>"><?= $category_name ?></option>
+												<?php
+												endforeach
+												?>
+											</select>
+										</div>
+										<div class="col-4">
+											<label class="form-label">Subcategory</label>
+											<select id="list-subcategory" class="form-control form-select" name="subcategory"></select>
+										</div>
+										<div class="col-4">
+											<li class="mb-2">
+												<label class="form-label">Price</label>
+												<input type="text" name="price" class="form-control" required>
+											</li>
+										</div>
+									</div>
+									<li class="mb-2">
+										<label class="form-label">Description</label>
+										<textarea name="description" class="form-control" required></textarea>
+									</li>
+									<li class="mb-2">
+										<label class="form-label">Manifacturer</label>
+										<input type="text" name="manifacturer" class="form-control" required>
+									</li>
+									<li class="mb-2">
+										<label class="form-label">Variants (optional)</label>
+										<input type="text" name="variant" class="form-control">
+									</li>
+									<li class="mb-2">
+										<label class="form-label">Weight (optional)</label>
+										<input type="number" name="weight" class="form-control" placeholder="0 kg">
+									</li>
+									<li class="mb-2">
+										<label class="form-label">Stock</label>
+										<input type="number" name="stock" class="form-control" placeholder="0" required>
+									</li>
+									<!-- <li class="mb-2">
+										<label class="form-label">Image</label>
+										<input class="form-control" type="file" name="image">
+									</li> -->
+									<li class="mt-3">
+										<a type="submit" href="index.php" class="btn_1">BACK</a>
+										<button type="submit" name="submit" class="btn_1">SAVE</button>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</form>
+				<?php
+				if (isset($_POST['submit'])) {
+					$email = $_SESSION['email'];
+					$get_user = get('user', 'WHERE email="'.$email.'"');
+					$data_user = mysqli_fetch_assoc($get_user);
+
+					$product_name = $_POST['name'];
+					$seller_id = $data_user['user_id'];
+					$category = $_POST['category'];
+					$subcategory = $_POST['subcategory'];
+					$price = $_POST['price'];
+					$stock = $_POST['stock'];
+					$description = $_POST['description'];
+					$variant = $_POST['variant'];
+					$weight = $_POST['weight'];
+
+					// Optional
+					if ($variant == '') {
+						$variant = 1;
+					} elseif ($weight == '') {
+						$weight = 0;
+					}
+
+					$manifacturer = $_POST['manifacturer'];
+					$slug = strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '-', $manifacturer)));
+					$select = get('manifacturer', 'WHERE slug="' . $slug . '"');
+					if (mysqli_num_rows($select) > 0) {
+						$data = mysqli_fetch_assoc($select);
+						$id = $data['manifacturer_id'];
+					} else {
+						insert('manifacturer', [
+							'manifacturer_name' => $manifacturer,
+							'slug' => $slug
+						]);
+						$id = mysqli_insert_id($connect);
+					}
+
+					$result = insert('product', [
+						'product_name' => $product_name,
+						'seller_id' => $seller_id,
+						'category_id' => $category,
+						'subcategory_id' => $subcategory,
+						'price' => $price,
+						'stock' => $stock,
+						'description' => $description,
+						'manifacturer_id' => $id,
+						'variant' => $variant,
+						'weight' => $weight
+					]);
+
+					$id = $connect -> insert_id;
+
+					if (!empty($_FILES['image']['name'])) {
+						$name = $_FILES['image']['name'];
+
+						$explode = explode(".", $name);
+						list($file_name, $extension) = $explode;
+						$image_name = time() . "." . $extension;
+
+						$tmp = $_FILES['image']['tmp_name'];
+						if (move_uploaded_file($tmp, "uploads/product/" . $image_name)) {
+							$query = "INSERT INTO product_image (image_name, product_id) VALUES ('" . $image_name . "', '" . $id . "')";
+							$result = mysqli_query($connect, $query);
+
+							if (!$result) {
+								unlink("uploads/product/" . $image_name);
+							}
+						}
+					}
+
+					if ($result) {
+						echo '<script>window.location.href = "index.php"</script>';
+					}
+				}
+				?>
+			</div>
+		</main>
+
+		<div id="toTop"></div> <!-- Back to top button -->
+
+		<!-- COMMON SCRIPTS -->
+		<script src="js/common_scripts.min.js"></script>
+		<script src="js/main.js"></script>
+		<script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
+		<script>
+			function getcategory(categoryId) {
+				$.ajax({
+					type: "get",
+					url: "page/product/data/add.php?category_id=" + categoryId,
+					dataType: "html",
+					success: function(response) {
+						$('#list-subcategory').html(response);
+					}
+				});
+			}
+
+			$(document).ready(function() {
+				let idcategory = $('#list-category').val();
+
+				getcategory(idcategory);
+
+				$('#list-category').change(function(e) {
+					e.preventDefault();
+					let idcategory = $('#list-category').val();
+
+					getcategory(idcategory);
+				});
+			});
+		</script>
+</body>
+</html>
---
+++ page/seller/index.php
@@ -0,0 +1,243 @@
+<?php
+$email = $_SESSION['email'];
+
+$get_user = get('user', 'WHERE email="' . $email . '"');
+$data_user = mysqli_fetch_assoc($get_user);
+
+$user_id = $data_user['user_id'];
+$name = $data_user['user_name'];
+$email = $data_user['email'];
+$address = $data_user['address'];
+$country_id = $data_user['country_id'];
+$telephone = $data_user['telephone'];
+
+$get_country = get('country', 'WHERE country_id=' . $country_id);
+$data_country = mysqli_fetch_assoc($get_country);
+$country_name = $data_country['country_name'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="Ansonika">
+	<title>Allaia | Bootstrap eCommerce Template - ThemeForest</title>
+
+	<!-- Favicons-->
+	<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+	<link rel="apple-touch-icon" type="image/x-icon" href="img/apple-touch-icon-57x57-precomposed.png">
+	<link rel="apple-touch-icon" type="image/x-icon" sizes="72x72" href="img/apple-touch-icon-72x72-precomposed.png">
+	<link rel="apple-touch-icon" type="image/x-icon" sizes="114x114" href="img/apple-touch-icon-114x114-precomposed.png">
+	<link rel="apple-touch-icon" type="image/x-icon" sizes="144x144" href="img/apple-touch-icon-144x144-precomposed.png">
+
+	<!-- GOOGLE WEB FONT -->
+	<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900&display=swap" rel="stylesheet">
+
+	<!-- BASE CSS -->
+	<link href="css/bootstrap.custom.min.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+
+	<!-- SPECIFIC CSS -->
+	<link href="css/cart.css" rel="stylesheet">
+
+	<!-- YOUR CUSTOM CSS -->
+	<link href="css/custom.css" rel="stylesheet">
+
+</head>
+
+<style>
+	.text-link:hover {
+		text-decoration: underline;
+	}
+</style>
+
+<body>
+	<div id="page">
+		<main class="bg_gray">
+			<div class="container margin_30 pb-0">
+				<div class="page_header">
+					<div class="breadcrumbs">
+						<ul>
+							<li><a href="#">Home</a></li>
+							<li><a href="#">Seller</a></li>
+							<li>Dashboard</li>
+						</ul>
+					</div>
+					<h1 class="pt-3">Dashboard</h1>
+				</div>
+			</div>
+			<div class="container">
+				<div class="row">
+					<div class="col-3">
+						<?php
+						$result = get('user_image', 'WHERE user_id=' . $user_id);
+
+						if (mysqli_num_rows($result) > 0) :
+							$data = mysqli_fetch_assoc($result);
+							$user_image = $data['user_image'];
+						?>
+							<img src="uploads/user/<?= $user_image ?>" class="lazy" style="border-radius:50%" alt="user_image" width="100%">
+						<?php
+						else :
+						?>
+							<img src="uploads/user/default.jpg" class="lazy" style="border-radius:50%" alt="user_image" width="100%">
+						<?php endif ?>
+					</div>
+					<div class="col-9">
+						<ul style="list-style: none;" class="pl-4">
+							<li>
+								<h1 class="mb-3"><?= $name ?></h1>
+							</li>
+							<li>
+								<h5>Address</h5>
+								<p><?= $address ?>, <?= $country_name ?></p>
+							</li>
+							<li>
+								<h5>Telephone</h5>
+								<p><?= $telephone ?></p>
+							</li>
+							<li>
+								<a href="index.php?page=product_add" class="btn_1">Add New Product</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="container margin_60_35">
+				<h5 class="mb-4">Your Products</h5>
+				<a href="index.php?page=seller_product">
+					<p class="m-0 text-right text-link">View All</p>
+				</a>
+				<div class="owl-carousel owl-theme products_carousel">
+					<?php
+					$get_product = get('product', 'WHERE seller_id='.$user_id);
+
+					foreach ($get_product as $data) :
+						$product_id = $data['product_id'];
+
+						$get_product = get('product', 'WHERE product_id=' . $product_id);
+						$data = mysqli_fetch_assoc($get_product);
+
+						$product_name = $data['product_name'];
+						$category_id = $data['category_id'];
+						$subcategory_id = $data['subcategory_id'];
+						$price = $data['price'];
+						$sold = $data['sold'];
+						$description = $data['description'];
+					?>
+						<div class="item">
+							<div class="grid_item">
+								<!-- <span class="ribbon hot">Hot</span>
+							<span class="ribbon off">-30%</span>
+							<span class="ribbon new">New</span> -->
+								<?php
+								$get_sale = get('sale', 'WHERE product_id=' . $product_id);
+								if (mysqli_num_rows($get_sale) > 0) :
+									$data_sale = mysqli_fetch_assoc($get_sale);
+									$sale = $data_sale['sale'];
+									$price_sale = $price - $price * (int) $sale / 100;
+								?>
+									<span class="ribbon off">- <?= $sale ?>%</span>
+								<?php endif ?>
+								<figure>
+									<a href="index.php?page=product_view&product_id=<?= $product_id ?>">
+										<?php
+										$result = get('product_image', 'WHERE product_id=' . $product_id);
+										if (mysqli_num_rows($result) > 0) :
+											$data = mysqli_fetch_assoc($result);
+											$image_name = $data['image_name'];
+										?>
+											<img src="uploads/product/<?= $image_name ?>" class="lazy" width="100%" style="width: 250px; height: 250px; object-fit: scale-down;">
+										<?php
+										else :
+										?>
+											<img src="img/products/product_placeholder_square_medium.jpg" class="lazy" width="100%" style="width: 250px; height: 250px; object-fit: scale-down;">
+										<?php endif ?>
+									</a>
+								</figure>
+								<div class="rating">
+									<?php
+									$get_review = get('review', 'WHERE product_id="' . $product_id . '"', 'sum(rating)');
+									$data_review = mysqli_fetch_assoc($get_review);
+									$total_rating = (int)$data_review['sum(rating)'];
+
+									$get_review = get('review', 'WHERE product_id="' . $product_id . '"', 'count(rating)');
+									$data_review = mysqli_fetch_assoc($get_review);
+									$count_rating = (int)$data_review['count(rating)'];
+
+									if ($count_rating > 0) {
+										$average_rating = round($total_rating / $count_rating);
+
+										for ($i = $average_rating; $i > 0; $i--) {
+											echo '<i class="icon-star voted"></i>';
+										}
+									}
+
+									if ($count_rating == 0) {
+										echo '<p class="mb-0">No review</p>';
+									} else {
+										$n = 5 - $average_rating;
+										for ($i = $n; $i > 0; $i--) {
+											echo '<i class="icon-star"></i>';
+										}
+										echo '<em class="ml-2" style="color:#9d9d9d">(' . $count_rating . ')</em>';
+									}
+									?>
+								</div>
+								<a href="index.php?page=product_view&product_id=<?= $product_id ?>">
+									<h3>
+										<?= $product_name ?>
+									</h3>
+								</a>
+								<div class="price_box mb-0">
+									<?php
+									$get_sale = get('sale', 'WHERE product_id=' . $product_id);
+									if (mysqli_num_rows($get_sale) > 0) :
+										$data_sale = mysqli_fetch_assoc($get_sale);
+										$sale = $data_sale['sale'];
+										$price_sale = $price - $price * (int) $sale / 100;
+									?>
+										<span class="new_price">
+											<?= rupiah($price_sale) ?>
+										</span>
+										<span class="old_price" style="font-size:small">
+											<?= rupiah($price) ?>
+										</span>
+									<?php else : ?>
+										<span class="new_price">
+											<?= rupiah($price) ?>
+										</span>
+									<?php endif ?>
+								</div>
+								<div>
+									<p style="color: #9d9d9d;" class="mb-3">
+										<?= $sold ?> sold • 0 discussions
+									</p>
+								</div>
+								<ul>
+									<li>
+										<a href="index.php?page=product_delete&product_id=<?= $product_id ?>&quantity=1" class="tooltip-1" title="Delete Product" onclick="return confirm('Are you sure to DELETE this PRODUCT?')" data-toggle="tooltip" data-placement="left">
+											<i class="ti-trash"></i>
+										</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					<?php endforeach ?>
+				</div>
+			</div>
+	</div>
+	</main>
+
+	<div id="toTop"></div><!-- Back to top button -->
+
+	<!-- COMMON SCRIPTS -->
+	<script src="js/common_scripts.min.js"></script>
+	<script src="js/main.js"></script>
+</body>
+
+</html>
