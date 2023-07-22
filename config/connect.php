238b9ef170cecc19c4e80ef468c924d8fd2f5833
Ahmad Zaidaan <zaidaan2019@gmail.com>
2023-07-22 15:04:10 +0700

    added postal code

--- config/connect.php
+++ config/connect.php
@@ -9,29 +9,31 @@ $dbname = 'online_shop';
 $connect = mysqli_connect($severname, $username, $password, $dbname);
 
 if (!$connect) {
-    die('Failed to Connect : '.mysqli_connect_error());
+    die('Failed to Connect : ' . mysqli_connect_error());
 }
 
 // ================== Function ==================
 
-function get($table, $condition='', $select='*') {
+function get($table, $condition = '', $select = '*')
+{
     global $connect;
 
-    $query = "SELECT ".$select." FROM ".$table." ".$condition;
+    $query = "SELECT " . $select . " FROM " . $table . " " . $condition;
     $result = mysqli_query($connect, $query);
 
-    return $result; 
+    return $result;
 }
 
-function login($email, $password) {
-    $result = get("user","WHERE email='".$email."'");
+function login($email, $password)
+{
+    $result = get("user", "WHERE email='" . $email . "'");
 
     if (mysqli_num_rows($result) > 0) {
         $result = mysqli_fetch_assoc($result);
         if (password_verify($password, $result['password'])) {
             $_SESSION['email'] = $result['email'];
 
-            // echo '<script>alert("Successfully login!")</script>';
+            // echo '<script>alert("Successfully login!")</>';
             echo '<script>window.location.href = "index.php"</script>';
         } else {
             echo '<script>alert("Password incorrect")</script>';
@@ -41,9 +43,10 @@ function login($email, $password) {
     }
 }
 
-function register($user_name, $role, $email, $password, $confirmPassword, $address, $country_id, $telephone) {
-    $result = get("user","WHERE email='".$email."'");
-    
+function register($user_name, $role, $email, $password, $confirmPassword, $address, $country_id, $postal_code, $phone)
+{
+    $result = get("user", "WHERE email='" . $email . "'");
+
     if (mysqli_num_rows($result) == 0) {
         if ($password == $confirmPassword) {
             $password_encrypt = password_hash($password, PASSWORD_DEFAULT);
@@ -54,7 +57,8 @@ function register($user_name, $role, $email, $password, $confirmPassword, $addre
                 'password' => $password_encrypt,
                 'address' => $address,
                 'country_id' => $country_id,
-                'telephone' => $telephone
+                'postal_code' => $postal_code,
+                'phone' => $phone
             ]);
             if ($result) {
                 $_SESSION['email'] = $email;
@@ -68,7 +72,8 @@ function register($user_name, $role, $email, $password, $confirmPassword, $addre
     }
 }
 
-function check($check) {
+function check($check)
+{
     switch ($check) {
         case 'login':
             if (!isset($_SESSION['email'])) {
@@ -83,38 +88,42 @@ function check($check) {
     }
 }
 
-function log_out() {
+function log_out()
+{
     session_destroy();
     echo '<script>window.location.href = "index.php?page=login"</script>';
 }
 
-function wishlist($email, $product_id) {
+function wishlist($email, $product_id)
+{
     global $connect;
 
-    $result = get("user", "WHERE id='".$email."'");
+    $result = get("user", "WHERE id='" . $email . "'");
     $data = mysqli_fetch_assoc($result);
 
-    $query = "UPDATE user SET wishlist='".$product_id."' WHERE id='".$email."'";
+    $query = "UPDATE user SET wishlist='" . $product_id . "' WHERE id='" . $email . "'";
     $result = mysqli_query($connect, $query);
 }
 
-function rupiah($angka) {
-    $hasil = 'Rp'.number_format($angka, 2, ',', '.');
+function rupiah($angka)
+{
+    $hasil = 'Rp' . number_format($angka, 2, ',', '.');
     return $hasil;
 }
 
-function insert($table, $data) {
+function insert($table, $data)
+{
     global $connect;
 
     $keys = array_keys($data);
-    $values = array_map(function($values) use ($connect) {
-        return "'".mysqli_real_escape_string($connect, $values)."'";
+    $values = array_map(function ($values) use ($connect) {
+        return "'" . mysqli_real_escape_string($connect, $values) . "'";
     }, array_values($data));
-    
-    $query = "INSERT INTO $table (".implode(",", $keys).") VALUES (".implode(",", $values).")";
-    
+
+    $query = "INSERT INTO $table (" . implode(",", $keys) . ") VALUES (" . implode(",", $values) . ")";
+
     // var_dump($query); die;
-    
+
     if (mysqli_query($connect, $query)) {
         return true;
     } else {
@@ -122,7 +131,8 @@ function insert($table, $data) {
     }
 }
 
-function dateConvert($input) {
+function dateConvert($input)
+{
     $explode = explode('-', $input);
     $output = '';
 
@@ -166,12 +176,13 @@ function dateConvert($input) {
         $month = 'December';
     }
 
-    $output = $day.' '.$month.', '.$year;
-    
+    $output = $day . ' ' . $month . ', ' . $year;
+
     return $output;
 }
 
-function cutFromEnd($string, $value) {
+function cutFromEnd($string, $value)
+{
     $result = rtrim($string, substr($string, -$value));
     return $result;
 }
