5df81e44d5dfa8c0b2d92da7d2fdfa3da19c03c1
Ahmad Zaidaan <zaidaan2019@gmail.com>
2023-07-22 15:02:41 +0700

    added read more

--- page/seller/product/list.php
+++ page/seller/product/list.php
@@ -1,15 +1,17 @@
 <?php
 if (isset($_SESSION['email'])) {
 	$email = $_SESSION['email'];
-	
+
 	$get_user = get('user', 'WHERE email="' . $email . '"');
 	$data_user = mysqli_fetch_assoc($get_user);
-	
+
 	$role = $data_user['role'];
-	
+
 	if ($role != 'seller') {
 		echo '<script>window.location.href = "index.php"</script>';
 	}
+
+	$user_id = $data_user['user_id'];
 }
 ?>
 
@@ -41,6 +43,20 @@ if (isset($_SESSION['email'])) {
 	<link href="css/custom.css" rel="stylesheet">
 </head>
 
+<style>
+	#more {
+		display: none;
+	}
+
+	.product:hover {
+		text-decoration: underline;
+	}
+
+	.product-image img:hover {
+		opacity: 75%;
+	}
+</style>
+
 <body>
 	<div id="page">
 		<main class="bg_gray">
@@ -67,7 +83,7 @@ if (isset($_SESSION['email'])) {
 				<table class="table table-striped table-hover table-sm">
 					<tbody>
 						<?php
-						$result = get('product');
+						$result = get('product', 'WHERE seller_id=' . $user_id);
 						foreach ($result as $data) :
 							$product_id = $data['product_id'];
 							$product_name = $data['product_name'];
@@ -89,38 +105,65 @@ if (isset($_SESSION['email'])) {
 								<td>
 									<div class="row">
 										<div class="col-3">
-											<?php
-											$result = get('product_image', 'WHERE product_id=' . $product_id . ' ORDER BY image_index DESC');
-											if (mysqli_num_rows($result) > 0) :
-												$data = mysqli_fetch_assoc($result);
-												$image_name = $data['image_name'];
-											?>
-												<img src="uploads/product/<?= $image_name ?>" alt="product_image" style="width:250px; height:250px; object-fit:scale-down">
-											<?php
-											else :
-											?>
-												<img src="uploads/product/default.jpg" alt="product_image" style="width:250px; height:250px; object-fit:scale-down">
-											<?php endif ?>
+											<a class="product-image" href="index.php?page=product_view&product_id=<?= $product_id ?>">
+												<?php
+												$result = get('product_image', 'WHERE product_id=' . $product_id . ' ORDER BY image_index DESC');
+												if (mysqli_num_rows($result) > 0) :
+													$data = mysqli_fetch_assoc($result);
+													$image_name = $data['image_name'];
+												?>
+													<img src="uploads/product/<?= $image_name ?>" alt="product_image" style="width:250px; height:250px; object-fit:scale-down">
+												<?php
+												else :
+												?>
+													<img src="uploads/product/default.jpg" alt="product_image" style="width:250px; height:250px; object-fit:scale-down">
+												<?php endif ?>
+											</a>
 										</div>
 										<div class="col-3 p-0">
-											<h4 class="mt-2"><?= $product_name ?></h5>
-												<?php
-												$get_sale = get('sale', 'WHERE product_id=' . $product_id);
-												if (mysqli_num_rows($get_sale) > 0) :
-													$data_sale = mysqli_fetch_assoc($get_sale);
-													$sale = $data_sale['sale'];
-													$price_sale = $price - $price * (int)$sale / 100;
-												?>
-													<span style="font-size:larger" class="new_price"><?= rupiah($price_sale) ?></span>
-													<span style="color:#9d9d9d" class="old_price"><?= rupiah($price) ?></span>
-												<?php else : ?>
-													<span style="font-size:larger" class="new_price"><?= rupiah($price) ?></span>
-												<?php endif ?>
-												<p>Stock: <?= $stock ?></p>
-												<p style="color:#9d9d9d"><?= $category_name ?> > <?= $subcategory_name ?></p>
+											<a href="index.php?page=product_view&product_id=<?= $product_id ?>">
+												<h4 class="mt-2 product"><?= $product_name ?></h5>
+											</a>
+											<?php
+											$get_sale = get('sale', 'WHERE product_id=' . $product_id);
+											if (mysqli_num_rows($get_sale) > 0) :
+												$data_sale = mysqli_fetch_assoc($get_sale);
+												$sale = $data_sale['sale'];
+												$price_sale = $price - $price * (int)$sale / 100;
+											?>
+												<span style="font-size:larger" class="new_price"><?= rupiah($price_sale) ?></span>
+												<span style="color:#9d9d9d" class="old_price"><?= rupiah($price) ?></span>
+											<?php else : ?>
+												<span style="font-size:larger" class="new_price"><?= rupiah($price) ?></span>
+											<?php endif ?>
+											<p>Stock: <?= $stock ?></p>
+											<p style="color:#9d9d9d"><?= $category_name ?> > <?= $subcategory_name ?></p>
 										</div>
-										<div class="col p-0">
-											<?= $description ?>
+										<div class="col pl-0 pr-4 pb-3">
+											<?php
+											$description_array = explode(' ', $description);
+											$description_count = count($description_array);
+
+											$description1 = '';
+											$description2 = '';
+
+											if ($description_count > 55) :
+												for ($i = 0; $i < 55; $i++) {
+													$description1 .= $description_array[$i] . ' ';
+												}
+												for ($i = 55; $i < $description_count; $i++) {
+													$description2 .= $description_array[$i] . ' ';
+												}
+											?>
+												<p class="m-0 description">
+													<?= $description1 ?>
+													<span id="dots">...</span>
+													<span id="more"><?= $description2 ?></span>
+												</p>
+												<button onclick="readMore(<?= $product_id ?>)" id="readmore<?= $product_id ?>" class="btn btn-link p-0" style="font-size:small;">read more</button>
+											<?php else : ?>
+												<p><?= $description ?></p>
+											<?php endif ?>
 										</div>
 										<div class="col-1 p-0" style="max-width:57px">
 											<div class="btn-group-vertical btn-group-sm">
@@ -145,6 +188,24 @@ if (isset($_SESSION['email'])) {
 				</div>
 			</div>
 		</main>
+
+		<script>
+			function readMore(i) {
+				let dots = document.getElementById("dots");
+				let moreText = document.getElementById("more");
+				let btnText = document.getElementById("readmore" + i);
+
+				if (dots.style.display == "none") {
+					dots.style.display = "inline";
+					btnText.innerHTML = "read more";
+					moreText.style.display = "none";
+				} else {
+					dots.style.display = "none";
+					btnText.innerHTML = "read less";
+					moreText.style.display = "inline";
+				}
+			}
+		</script>
 </body>
 
 </html>
