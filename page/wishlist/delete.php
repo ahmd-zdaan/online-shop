80ef921162f2b5659fdbab55b9ea5829f6285436
Ahmad Zaidaan <zaidaan2019@gmail.com>
2023-08-24 16:36:53 +0700

    fixed not deleting wishlist

--- page/wishlist/delete.php
+++ page/wishlist/delete.php
@@ -1,10 +1,13 @@
 <?php
 $user_email = $_SESSION['email'];
+$get_user = get('user', 'WHERE email="' . $user_email . '"');
+$data_user = mysqli_fetch_assoc($get_user);
+$user_id = $data_user['user_id'];
+
 $product_id = $_GET['product_id'];
 
-$query = "DELETE FROM wishlist WHERE user_id=".$user_id." AND product_id=".$product_id;
+$query = 'DELETE FROM wishlist WHERE user_id=' . $user_id . ' AND product_id=' . $product_id;
 
 if (mysqli_query($connect, $query)) {
     echo "<script>window.location.href = 'index.php?page=wishlist_list'</script>";
 }
-?>
